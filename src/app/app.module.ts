ac5f78f94260f2619b8c19b595afeb0ae40a802d
Kaciano Ghelere <kaciano_ghelere@yahoo.com.br>
2017-06-29 11:55:17 -0300

    HomeModule adicionado e routing inicial implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HomeModule } from './home/home.module';
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
+  imports: [
+    BrowserModule, HomeModule, AppRoutingModule
+  ],
   declarations: [
     AppComponent
-  ],
-  imports: [
-    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
